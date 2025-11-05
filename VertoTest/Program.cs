685635d60bdfe0bf47309f27c95812a1fe31e2cc
Sebastian Richards <sebastianrichards@me.com>
2025-11-05 23:23:24 +0000

    configure for subdomain on server

--- VertoTest/Program.cs
+++ VertoTest/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddDbContext<VertoTest.Models.ContentContext>(options =>
 
 var app = builder.Build();
 
+// Configure the app to work under /verto path
+app.UsePathBase("/verto");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
